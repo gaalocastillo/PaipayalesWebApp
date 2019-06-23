06369a37def22668fcfddcdd1ce1bf40d631bea2
soccerdroid <mguarand@espol.edu.ec>
2019-06-22 23:04:03 -0500

    ya no se centra mientras se actualiza el mapa.Borrado de botones de live updates completo.

--- static/tracks/js/base.js
+++
@@ -1,7 +0,0 @@
-var mymap = L.map('mapdiv').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-  maxZoom: 20,
-  attribution: '&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  crs: L.CRS.EPSG4326
-}).addTo(mymap);
