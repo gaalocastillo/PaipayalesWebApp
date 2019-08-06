a22bfca08ad4e4875b79322b3cab3756768e8141
Galo Castillo <gadacast@espol.edu.ec>
2019-08-06 08:57:27 -0500

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -8,11 +8,11 @@ from .views import *
 
 
 urlpatterns = [
-    url(r'^$', index, name="index"),
-    url(r'^(?P<product_id>[0-9]+)$', detail, name="detail"),
+    #url(r'^$', index, name="index"),
+    #url(r'^(?P<product_id>[0-9]+)$', detail, name="detail"),
     #url(r'^(?P<pk>[\w-]+)$', detail, name="detail"),
-    url(r'^edit/(?P<pk>[\w-]+)$', edit, name="edit"),
-    url(r'^addnew/$', addnew, name="addnew"),
+    #url(r'^edit/(?P<pk>[\w-]+)$', edit, name="edit"),
+    #url(r'^addnew/$', addnew, name="addnew"),
 
     url(r'^api/v1/products/(?P<cat>\w+)$', ListProductsView.as_view(), name="products-all"),
     url(r'^api/v1/users/user-zones/$', ListUserZonesView.as_view(), name="user-zones-all"),
